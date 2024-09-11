d8612cc7f756eb53d09e799a71399d9de4c08de1
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-09-11 13:32:30 +0100

    🚀 Preparation for Heroku backend deployment

--- backend/index.js
+++ backend/index.js
@@ -10,17 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
 // API route
 app.get('/api', (req, res) => {
   res.send('Welcome to WayGenie API');
-});
-
-// Serve the React app for any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
