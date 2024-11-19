d4e5ba1c82310e7b7c01308c8821f457da438fca
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-11-19 16:53:12 +0000

    Fixed css issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {
       fontFamily: {
-        DM: ["Fredoka", "sans-serif"],
+        'DM': ['Fredoka', 'sans-serif'],
       },
     },
   },
