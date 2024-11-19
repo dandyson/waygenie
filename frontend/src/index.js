5cdcbe5ceb44bfcd310087e04b5738146a681536
Dan Dyson <31185372+dandyson@users.noreply.github.com>
2024-11-19 16:49:52 +0000

    Auth0 Implementation: Configure Auth0Provider
    Set up Auth0 provider wrapper

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import './index.css';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE
+        }}
+        cacheLocation="localstorage"
+        useRefreshTokens={true}
+        skipRedirectCallback={window.location.pathname === '/callback'}
+      >
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </Auth0Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
